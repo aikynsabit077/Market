b542980a8c1dcd41c3cbd888fbae2deaf3e759ac
aikynsabit077 <aikyn.sabit.077@gmail.com>
2026-01-23 02:21:04 +0500

    Update market.java

--- src/market.java
+++ src/market.java
@@ -1,19 +1,1 @@
-public class market {
-    private String marketName;
-    private String ownerName;
-    private String address;
-    private int foundedYear;
 
-    public market(String marketName, String ownerName, String address, int foundedYear) {
-        this.marketName = marketName;
-        this.ownerName = ownerName;
-        this.address = address;
-        this.foundedYear = foundedYear;
-    }
-    public int getMarketAge() {
-        return 2025 - foundedYear;
-    }
-    public String getFullInfo() {
-        return "Market name: " + marketName + ", Owner: " + ownerName + ", Address: " + address + ", Founded: " + foundedYear;
-    }
-}
